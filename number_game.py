96ba1785bacdf1cf3f5319da02733ef7f61390f8
Lori Ortega <lori.ortega07@gmail.com>
2026-05-12 14:10:27 -0500

    Add files via upload
    Add the code for the Number_game created in PyCharm.

---
+++ number_game.py
@@ -0,0 +1,16 @@
+# Number game with repeating digits
+
+# 2-digit number
+num = int(input('Enter a 2 digit number: '))
+result = num * (3 * 7 * 13 * 37)
+print(result)
+
+# 3-digit number
+num = int(input('Enter a 3 digit number: '))
+result = num * (3 * 7 * 13 * 37)
+print(result)
+
+# 5-digit number
+num = int(input('Enter a 5 digit number: '))
+result = num * (3 * 7 * 13 * 37)
+print(result)
